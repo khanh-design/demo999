3332f2d074e6e5c110b8a3be8435e78a02c7481d
khanh-desgin <buiduykhanh1062003@gmail.com>
2025-04-10 23:43:45 +0700

    first commit

--- src/main/java/com/example/demo999/First.java
+++ src/main/java/com/example/demo999/First.java
@@ -1,4 +1,41 @@
 package com.example.demo999;
 
-public class First {
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "First", value = "/First")
+public class First implements Servlet {
+    ServletConfig config = null;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        this.config = config;
+        System.out.println("Servlet is initialized");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("");
+        out.println("Hello simple servlet");
+        out.println("");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "copyright 2007-1010";
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Servlet is destroyed");
+    }
 }
